6a815f638e14219033f60827ac74ff8ef26a77fb
Andrei <An.S-1@yandex.ru>
2024-06-07 15:39:28 +0300

    ch1_2

--- java_EPAM_ch1/src/main/java/ru/practicum/lesson1B/Lesson6.java
+++ java_EPAM_ch1/src/main/java/ru/practicum/lesson1B/Lesson6.java
@@ -1,9 +1,12 @@
 package ru.practicum.lesson1B;
 
+import ru.practicum.number.Number;
 import ru.practicum.number.NumberMeneger;
 
-/*
+import java.util.List;
 
+/*
+6. Простые числа.
  */
 public class Lesson6 extends LessonB {
     public Lesson6(NumberMeneger numberMeneger) {
@@ -12,6 +15,32 @@ public class Lesson6 extends LessonB {
 
     @Override
     public void game() {
+        showNumbers();
+        System.out.println("6. Простые числа.");
+        show(10, numberMeneger.getNumberList());
+    }
 
+    @Override
+    public void show(int length, List<Number> numbersList) {
+        int count = 0;
+        for (Number number : numbersList) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.floor(Math.sqrt(number.getNum())) ; i++) {
+                if (number.getNum() != i) {
+                    if (number.getNum() % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if (isPrime) {
+                System.out.print(number.getNum() + "\t");
+                count++;
+                if (count == length) {
+                    System.out.println();
+                    count = 0;
+                }
+            }
+        }
     }
 }
