f1d8d8b82be30abddb42456c90190b1b8aaa2959
Andrei <An.S-1@yandex.ru>
2024-06-09 14:37:49 +0300

    ch1_4

--- java_EPAM_ch1/src/main/java/ru/practicum/lesson1B/Lesson8.java
+++ java_EPAM_ch1/src/main/java/ru/practicum/lesson1B/Lesson8.java
@@ -1,10 +1,14 @@
 package ru.practicum.lesson1B;
 
+import ru.practicum.number.Number;
 import ru.practicum.number.NumberMeneger;
 
-/*
+import java.util.HashMap;
+import java.util.Map;
 
- */
+/*
+8. Числа в порядке убывания частоты встречаемости чисел.
+*/
 public class Lesson8 extends LessonB {
     public Lesson8(NumberMeneger numberMeneger) {
         super(numberMeneger);
@@ -12,6 +16,14 @@ public class Lesson8 extends LessonB {
 
     @Override
     public void game() {
-
+        showNumbers();
+        System.out.println("8. Числа в порядке возрастание частоты встречаемости чисел.");
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Number number : numberMeneger.getNumberList()) {
+            map.put(number.getNum(), map.getOrDefault(number.getNum(), 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
